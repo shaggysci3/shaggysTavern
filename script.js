be40afa87b300491624475b1d2bf31c35c64cd74
matthew <shaggysci3@gmail.com>
2023-09-29 06:32:17 -0400

    trying copy paste

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// creating an element and assighn it to a variable
-const h2 = document.createElement("h2)");
-h2.textContent = "this content is added by javaScript";
-// insert the element (h2) into the html dom. select body and append(change) to the child (lower branch)
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
